268df458e6d203863603d31df63be42913ad5ee7
John <github@johnturkson.com>
2020-01-11 14:58:05 -0800

    Update gradle build and settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     application
     kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.3.61"
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
 
 group = "com.johnturkson.test"
@@ -8,10 +12,17 @@ version = "0.1"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.ktor:ktor-server-cio:1.2.6")
+    implementation("io.ktor:ktor-serialization:1.2.6")
+    implementation("io.ktor:ktor-websockets:1.2.6")
 }
 
 application {
