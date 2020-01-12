fa9338f6319e8d6b3c357f52fa27705384faffc7
John <github@johnturkson.com>
2020-01-11 20:39:33 -0800

    Create request wrapper classes

---
+++ src/main/kotlin/otdr/backend/api/Requests.kt
@@ -0,0 +1,58 @@
+package otdr.backend.api
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Request
+
+@Serializable
+@SerialName("LoginRequest")
+data class LoginRequest(
+    val email: String,
+    val password: String
+) : Request()
+
+@Serializable
+sealed class GetRequest : Request()
+
+@Serializable
+@SerialName("GetMessageRequest")
+data class GetMessageRequest(
+    val messageID: String
+) : GetRequest()
+
+@Serializable
+@SerialName("GetUserRequest")
+data class GetUserRequest(
+    val userID: String
+) : GetRequest()
+
+@Serializable
+@SerialName("GetGroupRequest")
+data class GetGroupRequest(
+    val groupID: String
+) : GetRequest()
+
+@Serializable
+sealed class CreateRequest : Request()
+
+@Serializable
+@SerialName("CreateUserRequest")
+data class CreateUserRequest(
+    val id: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val password: String
+) : CreateRequest()
+
+@Serializable
+sealed class FindRequest : Request()
+
+@Serializable
+@SerialName("FindUserRequest")
+data class FindUserRequest(
+    val selector: UserSelector
+) : FindRequest()
